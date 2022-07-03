059bf55eac33b17cd212912667ee757ce860265c
Ovishek Paul <ovishek.paul@gmail.com>
2022-07-03 10:19:17 +0600

    change text to 'Learning webpack is fun!'

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import imageUrl from "../asset/image.png";
 const component = () => {
   const helloDiv = document.createElement("div");
   helloDiv.innerHTML = _.join(
-    ["<h1> Hello from Webpack! </h1>", "<p> Webpack is in the process! </p>"],
+    ["<h1> Hello from Webpack! </h1>", "<p> Learning webpack is fun! </p>"],
     ""
   );
   return helloDiv;
